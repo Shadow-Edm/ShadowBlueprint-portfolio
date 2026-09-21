7c4071e4acf89f9c24b3b61a18c639527426d7fd
Shadow-Edm <djiahaedmond@gmail.com>
2026-09-21 10:40:38 +0100

    fix: erreur sur la section Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -231,7 +231,7 @@ function Projects() {
                     <span className="h-1 w-1 rounded-full bg-slate-300" />
 
                     <span className="text-sm text-slate-500">
-                      Projet académique / personnel
+                      Projet personnel
                     </span>
 
                   </div>
